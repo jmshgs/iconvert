d01ea1be9a627af32466fc7b31ce7a9904ef0ce5
jxhug <james@jxhug.me>
2023-12-23 11:49:39 -0800

    Initial Commit

---
+++ iconvert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iconvert
+//
+//  Created by jxhug on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iconvert/iconvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iconvertApp.swift
+//  iconvert
+//
+//  Created by jxhug on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct iconvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
